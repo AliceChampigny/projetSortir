3aadc6f77fbd99e5f1303852edaf8ce0ba763a9c
fterrien2022 <francois.terrien2022@campus-eni.Fr>
2023-03-27 17:29:17 +0200

    SortieFormType

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieFormType;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
-    public function index(): Response
-    {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
-        ]);
+    #[Route('/ajouter',
+        name: 'ajouter')]
+    public function ajouterunesortie(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): Response {
+
+        $sortie = new Sortie();
+
+        $sortieForm = $this->createForm(SortieFormType::class, $sortie);
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+//
+            return $this->redirectToRoute('main_accueil');
+        }
+
+        return $this->render('sortie/ajouterunesortie.html.twig',
+            compact('sortieForm'));
     }
 }
