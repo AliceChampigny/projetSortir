ac1fbbec4b56cefe863d66793018cb9eb2d68c47
mlawson2022 <melodie.lawson2022@campus-eni.fr>
2023-03-28 09:28:51 +0200

    security revue pour limiter acces au page, et form de modif : ajout mdp

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Participant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,13 +15,18 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class)
-            ->add('prenom',TextType::class)
-            ->add('pseudo',TextType::class)
-            ->add('email',TextType::class)
-//            ->add('password')
-            ->add('telephone',TextType::class)
-
+            ->add('nom',TextType::class,['label'=>'Nom '])
+            ->add('prenom',TextType::class,['label'=>'Prénom '])
+            ->add('pseudo',TextType::class,['label'=>'Pseudo '])
+            ->add('email',TextType::class,['label'=>'E-mail '])
+            ->add('telephone',TextType::class,['label'=>'Téléphone '])
+            ->add('password',RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe doit être renseigné',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe '],
+                'second_options' => ['label' => 'Confirmation du mot de passe ']])
         ;
     }
 
