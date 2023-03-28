5e56f5713214a7f51b49b18abc7e6e6f73171484
fterrien2022 <francois.terrien2022@campus-eni.Fr>
2023-03-28 17:22:56 +0200

    AjoutSortie dans base de données

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use App\Form\SortieFormType;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -23,11 +27,13 @@ class SortieController extends AbstractController
             'sorties' => $sorties
         ]);
     }
-    #[Route('/ajouter',
+    #[Route('/ajouter/{organisateur}',
         name: 'ajouter')]
     public function ajouterunesortie(
         Request $request,
         EntityManagerInterface $entityManager,
+        Participant $organisateur,
+
     ): Response {
 
         $sortie = new Sortie();
@@ -36,10 +42,50 @@ class SortieController extends AbstractController
         $sortieForm->handleRequest($request);
 
         if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            try {
+//                $lieu = new Lieu();
+//                $lieu->setNom($lieu);
+//                $lieu->setRue('1, rue Franklin');
+//                $lieu->setLatitude(15);
+//                $lieu->setLongitude(15);
+
+                $ville = new Ville();
+                $ville->setNom('Nantes');
+                $ville->setCodePostal('44300');
+
+                $ville->addLieux($sortie->getLieu());
+
+                $etat = new Etat();
+                $etat->setLibelle('Créée');
+
+
+                $sortie->setOrganisateur($organisateur);
+                $sortie->setCampus($organisateur->getCampus());
+
+dump($sortie);
+
+            $entityManager->persist($ville);
+//            $entityManager->persist($lieu);
+            $entityManager->persist($etat);
+
+            $sortie->setEtat($etat);
+//            $sortie->setLieu($lieu);
+//            $lieu->setVille($ville);
+
             $entityManager->persist($sortie);
             $entityManager->flush();
 
-            return $this->redirectToRoute('main_accueil');
+
+                $this->addFlash('success', "Votre sortie a été ajoutée" );
+            return $this->redirectToRoute('participant_accueilcnte');
+
+            } catch (\Exception $exception){
+                $this->addFlash('danger', "Votre sortie n'a pas été ajoutée". $exception->getMessage() );
+                return $this->redirectToRoute('ajouter',[
+                    'organisateur'=>$organisateur->getId()
+                ]);
+            }
+
         }
 
         return $this->render('sortie/ajouterunesortie.html.twig',
