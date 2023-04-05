62326f73ead0bba7a10a40db9a63ca0b4ac7615b
fterrien2022 <francois.terrien2022@campus-eni.Fr>
2023-04-05 17:02:47 +0200

    Mise en Page Accueil

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -19,50 +19,76 @@ class FilterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-
             ->add("campus", EntityType::class, [
                 "required" => false,
+                'label' => '',
                 "class" => Campus::class,
-                "choice_label" => "nom"
+                "choice_label" => "nom",
+                'attr' => array(
+                    'class' => 'input2'
+                )
+
             ])
-            ->add('keyWord',TextType::class, ['label'=>'Le nom de la sortie contient :  ', 'required' => false])
+            ->add('keyWord', TextType::class, ['label' => 'Le nom de la sortie contient :  ',
+                'attr' => array(
+                    'class' => 'input2',
+                    'placeholder' => ' Mots Clés'
+                ),
+                'required' => false])
             ->add('dateDebut', DateTimeType::class, [
                 'label' => 'Entre',
                 'date_label' => 'dateDebut',
                 'required' => false,
                 'widget' => 'single_text',
                 'html5' => true,
-                ])
+                'attr' => array(
+                    'class' => 'input2'
+                )
+            ])
             ->add('dateFin', DateTimeType::class, [
                 'label' => 'Et',
                 'date_label' => 'dateFin',
                 'required' => false,
                 'widget' => 'single_text',
                 'html5' => true,
+                'attr' => array(
+                    'class' => 'input2'
+                )
             ])
             ->add('organisateurSorties', CheckboxType::class, [
-                'label'    => "Sorties dont je suis l'organisateur/trice",
+                'label' => "Sorties dont je suis l'organisateur/trice",
                 'required' => false,
                 'data' => true,
+                'attr' => array(
+                    'class' => 'input2'
+                )
             ])
             ->add('inscritSorties', CheckboxType::class, [
-                'label'    => "Sorties auxquelles je suis inscrit/e ",
+                'label' => "Sorties auxquelles je suis inscrit/e ",
                 'required' => false,
                 'data' => true,
+                'attr' => array(
+                    'class' => 'input2'
+                )
             ])
             ->add('nonInscritSorties', CheckboxType::class, [
-                'label'    => "Sorties auxquelles je ne suis pas inscrit/e ",
+                'label' => "Sorties auxquelles je ne suis pas inscrit/e ",
                 'required' => false,
                 'data' => true,
+                'attr' => array(
+                    'class' => 'input2'
+                )
 
             ])
             ->add('sortiesPassees', CheckboxType::class, [
-                'label'    => "Sorties passées ",
+                'label' => "Sorties passées ",
                 'required' => false,
+                'attr' => array(
+                    'class' => 'input2'
+                )
 
             ])
-            ->add("Rechercher", SubmitType::class)
-        ;
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
